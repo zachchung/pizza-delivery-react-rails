491bb1eba95ed34c4ba1780a7fbc9d7855499da3
zach chung <chungchunluk@gmail.com>
2020-09-25 00:11:07 +0800

    moved scss to app/assets/stylesheets/pages

--- app/javascript/pizzas/App.js
+++ app/javascript/pizzas/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './styles/App.css';
+// import './styles/App.css';
 import data from './server/pizzas.json';
 import Pizza from './components/pizza';
 import Cart from './components/cart';
